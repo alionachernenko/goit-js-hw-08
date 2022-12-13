f56d80e1820f88ee7338895532a59e23179d6256
Aliona Chernenko <108655520+alionachernenko@users.noreply.github.com>
2022-12-13 18:55:40 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
 
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  emailInput: document.querySelector('input[name="email"]'),
+  messageInput: document.querySelector('textarea[name="message"]'),
+};
+
+populateFormData();
+
+refs.form.addEventListener('input', throttle(onFormElementsInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function populateFormData() {
+  const formValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!formValues) return;
+
+  refs.emailInput.value = formValues.email;
+  refs.messageInput.value = formValues.message;
+}
+
+function onFormElementsInput() {
+  formData.message = refs.messageInput.value;
+  formData.email = refs.emailInput.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  refs.form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
