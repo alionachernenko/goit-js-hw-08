0c8b30f55f0731d821f82e1106f15fd636c04b6e
Aliona Chernenko <108655520+alionachernenko@users.noreply.github.com>
2022-12-13 18:51:13 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//console
+const iframe = document.querySelector('iframe');
+const STORAGE_KEY = 'videoplayer-current-time';
 
+const player = new Player(iframe);
+
+setTimeToPlay();
+
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
+
+function setTimeToPlay() {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+}
+
+function onPlayerTimeUpdate() {
+  player.getCurrentTime().then(setTimeOnUpdate);
+}
+
+function setTimeOnUpdate(seconds) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
